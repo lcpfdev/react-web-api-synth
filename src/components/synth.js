c7866def32f692ce7f24f25b1f0ceb86332f90c0
Luka <lukafrance@gmail.com>
2022-07-25 23:24:36 +0100

    creating osc

--- src/components/synth.js
+++ src/components/synth.js
@@ -1,10 +1,27 @@
 import './synth.css'
 const Synth = () => {
+    const check = "This page is rendering correctly";
+
+    const audioContext = new AudioContext(); // Lets us use Audio API
+    let osc = audioContext.createOscillator(); // creates osc
+    osc.type = 'triangle'; // sets wave
+    osc.connect(audioContext.destination); //destination is speakers by default
+        if (audioContext.state === 'suspended') {
+            audioContext.resume()
+        }
+
+     
+  
+     
+    
+
     return(
-
-        <h1>Hello World</h1>
-
-
+        <div>
+        <h1>{check}</h1>
+        <button onClick={(e) => { }}>Play</button>
+        
+    
+        </div>
     );
 
 };
