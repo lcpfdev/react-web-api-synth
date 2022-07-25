c34d863bd4b492f54a5ecd03115e54f7e4efad2d
Luka <lukafrance@gmail.com>
2022-07-26 00:06:32 +0100

    web osc working

--- src/components/synth.js
+++ src/components/synth.js
@@ -1,26 +1,36 @@
 import './synth.css'
 const Synth = () => {
     const check = "This page is rendering correctly";
+    const audioContext = new AudioContext(); // lets us use Audio API
+    const osc = audioContext.createOscillator() // creates osc
+    osc.connect(audioContext.destination) // set to dest to speakers
 
-    const audioContext = new AudioContext(); // Lets us use Audio API
-    let osc = audioContext.createOscillator(); // creates osc
-    osc.type = 'triangle'; // sets wave
-    osc.connect(audioContext.destination); //destination is speakers by default
-        if (audioContext.state === 'suspended') {
-            audioContext.resume()
-        }
-
-     
-  
-     
+        function trianglePlay() {
+           
+            osc.type = 'triangle'; 
+        osc.start();
+        osc.stop(audioContext.currentTime + 1)
     
+     
+       
+    }
+    function sawPlay() {
+        osc.type = 'square'; 
+        osc.start();
+        osc.stop(audioContext.currentTime + 1)
+        
+ 
+    }
+
+
+
 
     return(
+        
         <div>
         <h1>{check}</h1>
-        <button onClick={(e) => { }}>Play</button>
-        
-    
+        <button onClick={trianglePlay}>triangle</button>
+        <button onClick={sawPlay}>square</button>
         </div>
     );
 
