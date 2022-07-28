9e4d445a5bcc016202d025283f23d93b7a968a3c
Luka <lukafrance@gmail.com>
2022-07-28 18:20:43 +0100

    added detune function

--- src/App.js
+++ src/App.js
@@ -15,18 +15,43 @@ gain1.connect(out);
 
 function App() {
   const [osc1Freq, setOsc1Freq] = useState(osc1.frequency.value)
-  const changeOsc1Freq = e => {
-    let {value} = e.target // taking value 
+  const [osc1Detune, setOsc1Detune] = useState(osc1.detune.value) // Accessing what is has currently
+
+  
+  const changeOsc1Freq = (e) => {
+    let {value} = e.target // taking value,sets prop
     setOsc1Freq(value);
     osc1.frequency.value = value
     // console.log(value)
   }
+
+  const changeOsc1Detune = (e) => {
+    let {value} = e.target; // sets prop
+    setOsc1Detune(value)
+    osc1.detune.value = value 
+     
+  }
+
+  // const changeOsc1Type = (e) => {
+  //   let {id} = e.target
+  //   console.log(id)
+
+  // }
+
+
+
   return (
     <div className="App">
     <h1>React web Synth</h1>
     <button onClick={() => osc1.start()}>start osc</button>
     <button onClick={() => osc1.stop()}>stop osc</button>
-    <Osc1 changeFreq={changeOsc1Freq} freq={osc1Freq} />
+    <Osc1 
+    changeFreq={changeOsc1Freq} 
+    freq={osc1Freq} 
+    changeDetune={changeOsc1Detune}
+    detune = {osc1Detune}
+    // changeType={osc1.}
+    />
       
     </div>
   );
--- src/components/Osc1.js
+++ src/components/Osc1.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const Osc1 = ({changeFreq, freq }) => {
+const Osc1 = ({changeFreq, freq, changeDetune, detune }) => {
   return (
     <div>
-        <input value={freq} onChange={changeFreq}type="range" id="frequency" max="5000"></input>
+      <h1>Oscilator 1</h1>
+      <p1>Set Frequency:</p1>
+        <input value={freq} 
+        onChange={changeFreq}
+        type="range" 
+        id="frequency" 
+        max="5000"></input>
+        <p1>Set Detune</p1>
+        <input 
+        value={detune} 
+        onChange={changeDetune}
+        type="range" 
+        id="detune" 
+        ></input>
+
+
+
+
     </div>
   )
 }
