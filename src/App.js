9a1121f82d77992e39d01fe9a781503bd8c125b8
Luka <lukafrance@gmail.com>
2022-07-28 15:19:35 +0100

    re arranged osc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-import Synth from './components/synth';
+import React from 'react';
+
+const audioContext = new AudioContext(); // Allows us to use api
+const out = audioContext.destination; // Sets audio to output to speakers
+
+const osc1 = audioContext.createOscillator(); // creates osc  
+const gain1 = audioContext.createGain() // sets osc to gain 
+
+osc1.connect(gain1);  // allows osc output
+gain1.connect(out);
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Synth />
+    <h1>React web Synth</h1>
+    <button onClick={() => osc1.start()}>start osc</button>
+    <button onClick={() => osc1.stop()}>stop osc</button>
       
     </div>
   );
