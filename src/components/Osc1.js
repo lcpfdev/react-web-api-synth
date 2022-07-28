19587e9b2336455fae432235f184b6c39fbf00e7
Luka <lukafrance@gmail.com>
2022-07-28 19:53:55 +0100

    updated settings

--- src/components/Osc1.js
+++ src/components/Osc1.js
@@ -1,40 +1,25 @@
 import React from 'react'
 import './Osc1.css';
 
-const Osc1 = ({changeFreq, freq, changeDetune, detune, changeType }) => {
+const Osc1 = ({ change, settings }) => {
   return (
-    <div>
-      
-      <h1>Oscilator 1</h1>
-      
+    <div>  
+      <h1>Oscilator 1</h1>   
       <p1>Set Frequency:</p1>
-        <input value={freq} 
-        onChange={changeFreq}
+        <input 
+        value={settings.freq} 
+        onChange={change}
         type="range" 
         id="frequency" 
         max="5000"></input>
         <p1>Set Detune:</p1>
         
         <input 
-        value={detune} 
-        onChange={changeDetune}
+        value={settings.detune} 
+        onChange={change}
         type="range" 
         id="detune" 
         ></input>
-
-
-        <p1>Change wave type:</p1>
-        <h3>Wave:</h3>
-        <button id="sine" onClick={changeType} className={``}>Sine</button>
-        <button id="triangle" onClick={changeType} className={``}>Triangle</button>
-        <button id="square" onClick={changeType} className={``}>square</button>
-        <button id="sawtooth" onClick={changeType} className={``}>sawtooth</button>
-     
-        
-
-
-
-
         </div>
 
 
