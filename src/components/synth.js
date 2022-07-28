8e762ee1fc0aa1458715594cbc8369eaa1faec3a
Luka <lukafrance@gmail.com>
2022-07-28 12:50:59 +0100

    updated slider

--- src/components/synth.js
+++ src/components/synth.js
@@ -1,37 +1,39 @@
 import './synth.css'
+// import { useState } from 'react';
 const Synth = () => {
+
     const check = "This page is rendering correctly";
     const audioContext = new AudioContext(); // lets us use Audio API
     const osc = audioContext.createOscillator() // creates osc
-    osc.connect(audioContext.destination) // set to dest to speakers
-
-        function trianglePlay() {
-           
-            osc.type = 'triangle'; 
-        osc.start();
-        osc.stop(audioContext.currentTime + 1)
+    let volume  = audioContext.createGain()
+    osc.connect(volume).connect(audioContext.destination) // set to dest to speakers
     
-     
-       
+  
+        function trianglePlay() {  
+        osc.type = 'triangle';
+        osc.frequency.value = 80;
+        osc.start();
+        osc.stop(audioContext.currentTime + 2)   
     }
     function sawPlay() {
-        osc.type = 'square'; 
+        osc.type = 'square';
+        osc.frequency.value = 80;
         osc.start();
-        osc.stop(audioContext.currentTime + 1)
-        
- 
+        osc.stop(audioContext.currentTime + 2)
     }
-
-
-
-
+ 
+ 
     return(
-        
         <div>
+
         <h1>{check}</h1>
         <button onClick={trianglePlay}>triangle</button>
-        <button onClick={sawPlay}>square</button>
-        <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+        <button onClick={sawPlay}>square</button> 
+        <input  type="range" id="volume" name="volume" min="0" max="10"></input>
+    
+
+
+    
         </div>
     );
 
