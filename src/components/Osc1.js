c330d5c32a53503e84a1c415dcdeff5ab2486eee
Luka <lukafrance@gmail.com>
2022-07-29 11:19:46 +0100

    switch osc buttons

--- src/components/Osc1.js
+++ src/components/Osc1.js
@@ -2,29 +2,33 @@ import React from 'react'
 import './Osc1.css';
 
 const Osc1 = ({ change, settings, changeType }) => {
-  return (
+  let { type, frequency, detune } = settings // destructuring
+   return (
     <div className='synth'>  
       <h1>Oscilator 1</h1>   
       <p1>Set Frequency:</p1>
         <input 
-        value={settings.freq} 
-        onChange={change}
+        value={frequency} 
+        onChange={change} 
         type="range" 
         id="frequency" 
         max="5000"></input>
         <p1>Set Detune:</p1>
         
         <input 
-        value={settings.detune} 
+        value={detune} 
         onChange={change}
         type="range" 
         id="detune" 
         ></input>
         <p1>Set Wave</p1>
-        <button id="sine" oncClick={changeType} className={``}>Sine</button>
-        <button id="triangle" oncClick={changeType} className={``}>triangle</button>
-        <button id="square" oncClick={changeType} className={``}>square</button>
-        <button id="sawtooth" oncClick={changeType} className={``}>sawtooth</button>
+        <button id="sine" onClick={changeType} className={`${type==="sine" && "active"}`}>sine</button>
+
+        <button id="triangle" onClick={changeType} className={`${type==="triangle" && "active"}`}>triangle</button>
+
+        <button id="square" onClick={changeType} className={`${type==="square" && "active"}`}>square</button>
+
+        <button id="sawtooth" onClick={changeType} className={`${type==="sawtooth" && "active"}`}>sawtooth</button>
 
 
         </div>
